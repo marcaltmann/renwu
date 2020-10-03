69f02406f742aeb04fc021d1bba1f7477e444f58
Marc Altmann <marcaltmann@posteo.de>
2020-10-03 21:44:17 +0200

    Use UIkit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+const UIkit = require('uikit');
+
+import('../stylesheets/main.scss');
